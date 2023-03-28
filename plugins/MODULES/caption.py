4529bb8f5c1f8ffdf4a606f4f96641dab4d8e1ca
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 01:31:48 +0530

    Create caption.py

---
+++ plugins/MODULES/caption.py
@@ -0,0 +1,73 @@
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import asyncio
+from pyrogram import filters
+
+from config import Config
+
+import os
+from info import LOGIN_CHHANEL
+
+
+class Config(object):
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+      API_ID = int(os.environ.get("API_ID", 12345))
+      API_HASH = os.environ.get("API_HASH")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "🔰🔰🔰🔰")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "kinzanoufal")
+
+
+
+
+usercaption_position = Config.CAPTION_POSITION
+caption_position = usercaption_position.lower()
+caption_text = Config.CAPTION_TEXT
+
+
+@Client.on_message(filters.chat(LOGIN_CHHANEL) & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, message):
+      try:
+         media = message.document or message.video or message.audio
+         caption_text = Config.CAPTION_TEXT
+      except:
+         caption_text = ""
+         pass 
+      if (message.document or message.video or message.audio): 
+          if message.caption:                        
+             file_caption = f"**{message.caption}**"                
+          else:
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:
+          if caption_position == "top":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = caption_text + "\n" + file_caption,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "bottom":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = file_caption + "\n" + caption_text,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "nil":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id,
+                 message_id = message.message_id,
+                 caption = caption_text, 
+                 parse_mode = "markdown"
+             ) 
+      except:
+          pass
+              
+                   
+      
