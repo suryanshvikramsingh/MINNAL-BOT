c639d2c90c82807534244d6c8aba8140d97acee1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 02:58:16 +0530

    Update Attach.py

--- plugins/MODULES/Attach.py
+++ plugins/MODULES/Attach.py
@@ -1,130 +1,18 @@
-import os
+import os 
+import json
+import string 
+import random
+import asyncio
+import aiofiles
+from random import choice
 import pyrogram
-from pyrogram import Client, filters
+from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-
-
-
-START_TEXT = """Hello {},
-I am a media or file in a message attach bot. \
-I can attach photo, video, audio etc. using their public links in a message.
-
-Made by @FayasNoushad"""
-HELP_TEXT = """**More Help**
-
-**Steps :-**
-- Just send a html or markdown message
-- Reply a link for attaching
-
-**Tips :-**
-• Use Telegram public channel or group message links
-• You can send any type of links for attaching
-
-Made by @FayasNoushad"""
-
-ABOUT_TEXT = """**About Me**
-
-- **Bot :** `Attach Bot`
-- **Developer :** [Fayas](https://github.com/FayasNoushad)
-- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
-- **Source :** [Attach-Bot](https://github.com/FayasNoushad/Attach-Bot)
-- **Language :** [Python3](https://python.org)
-- **Library :** [Pyrogram](https://pyrogram.org)"""
-
-START_BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
-            InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
-        ],
-        [
-            InlineKeyboardButton('Help', callback_data='help'),
-            InlineKeyboardButton('About', callback_data='about'),
-            InlineKeyboardButton('Close', callback_data='close')
-        ]
-    ]
-)
-
-HELP_BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Home', callback_data='home'),
-            InlineKeyboardButton('About', callback_data='about'),
-            InlineKeyboardButton('Close', callback_data='close')
-        ]
-    ]
-)
-
-ABOUT_BUTTONS = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton('Home', callback_data='home'),
-            InlineKeyboardButton('Help', callback_data='help'),
-            InlineKeyboardButton('Close', callback_data='close')
-        ]
-    ]
-)
-
-
-@Client.on_callback_query()
-async def cb_handler(bot, update):
-    if update.data == "home":
-        await update.message.edit_text(
-            text=START_TEXT.format(update.from_user.mention),
-            reply_markup=START_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "help":
-        await update.message.edit_text(
-            text=HELP_TEXT,
-            reply_markup=HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    elif update.data == "about":
-        await update.message.edit_text(
-            text=ABOUT_TEXT,
-            reply_markup=ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
-    else:
-        await update.message.delete()
-
-
-@Client.on_message(filters.private & filters.command(["astart"]))
-async def start(bot, update):
-    await update.reply_text(
-        text=START_TEXT.format(update.from_user.mention),
-        disable_web_page_preview=True,
-        reply_markup=START_BUTTONS
-    )
-
-
-@Client.on_message(filters.private & filters.command(["help"]))
-async def help(bot, update):
-    await update.reply_text(
-        text=HELP_TEXT,
-        disable_web_page_preview=True,
-        reply_markup=HELP_BUTTONS
-    )
-
-
-@Client.on_message(filters.private & filters.command(["about"]))
-async def about(bot, update):
-    await update.reply_text(
-        text=ABOUT_TEXT,
-        disable_web_page_preview=True,
-        reply_markup=ABOUT_BUTTONS
-    )
-
 
 @Client.on_message(filters.text & filters.media & filters.private & filters.reply & filters.regex(r'https?://[^\s]+'))
 async def attach(bot, update):
-    await update.reply_text(
-        text=f"[\u2063]({update.text}){update.reply_to_message.text}",
-        reply_markup=update.reply_to_message.reply_markup
+    shit = await update.reply_text("**Processing...UR Request 🙂**")
+    await shit.edit_text(
+	    text=f"[\u2063]({update.text}){update.reply_to_message.text}",
+	    reply_markup=update.reply_to_message.reply_markup
     )
-
-
-
