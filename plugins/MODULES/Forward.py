ee16dbd32fcb81af55e2b1aaa3087f3875bfd761
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-28 23:54:39 +0530

    Update and rename quote.py to Forward.py

---
+++ plugins/MODULES/Forward.py
@@ -0,0 +1,24 @@
+import pyrogram
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+
+bughunter0 = Client(
+    "botname",
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"],
+)
+
+
+
+
+@Client.on_message(filters.forwarded & filters.channel)
+async def channeltag(bot, message):
+   try:
+       chat_id = message.chat.id
+       forward_msg = await message.copy(chat_id)
+       await message.delete()
+   except:
+       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+    
