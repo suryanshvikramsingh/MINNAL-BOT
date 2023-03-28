55f6613a776cef7a0c19ef5f49e5491d5188e666
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-28 16:19:04 +0530

    Update rules.py

--- plugins/EXTRA/rules.py
+++ plugins/EXTRA/rules.py
@@ -136,12 +136,12 @@ async def r_message(client, message):
 
 
 
-@Client.on_message(filters.reply) 
+@Client.on_message(filters.command("r"))
 async def start_message(client, message):
 #    mention = message.from_user.mention
 #    chat_id = message.chat.id
 #    mv_rqst = message.text
-    searchh = message.text                 
+    search = message.text                 
 #    reqstr1 = message.from_user.id if message.from_user else 0
 #    reqstr = await client.get_users(reqstr1)   
     imdb = await get_poster(searchh) if IMDB else None 
