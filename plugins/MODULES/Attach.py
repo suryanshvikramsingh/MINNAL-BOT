3fadf6fac012b071a46c203b1ea6deb83dc36f6f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 00:49:26 +0530

    Update Attach.py

--- plugins/MODULES/Attach.py
+++ plugins/MODULES/Attach.py
@@ -1,16 +1,19 @@
-# Author: Fayas (https://github.com/FayasNoushad) (@FayasNoushad)
+# (c) @KoshikKumar17
+import os 
+import json
+import string 
+import random
+import asyncio
+import aiofiles
+from random import choice
+import pyrogram
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-
-@Client.on_message(filters.command(["attach"]) & filters.reply, group=1)
-async def attach(update: Message):
-    if len(update.text.split()) <= 1:
-        await update.reply_text("Send command with link for attaching")
-        return
-    link = update.text.split(" ", 1)[1]
-    await update.reply_text(
-        text=f"[\u2063]({link}){update.reply_to_message.text}",
-        reply_markup=update.reply_to_message.reply_markup
+@Client.on_message(filters.text & filters.private & filters.reply & filters.regex(r'https?://[^\s]+'))
+async def attach(bot, update):
+    shit = await update.reply_text("**Processing...UR Request 🙂**")
+    await shit.edit_text(
+	    text=f"[\u2063]({update.text}){update.reply_to_message.text}",
+	    reply_markup=update.reply_to_message.reply_markup
     )
