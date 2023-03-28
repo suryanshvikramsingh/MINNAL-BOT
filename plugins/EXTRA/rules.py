ac3b7d1c26513bfedfbd088d2218576b3fdc472d
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-28 15:34:13 +0530

    Update rules.py

--- plugins/EXTRA/rules.py
+++ plugins/EXTRA/rules.py
@@ -117,7 +117,7 @@ async def start_message(client, message):
                                       
 
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-
+            imdb = await get_poster(searchh) if IMDB else None
             pic = imdb.get('poster')
 
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
@@ -128,9 +128,7 @@ async def start_message(client, message):
 
             logger.exception(e)
 
-            fmsg = await message.reply_photo(
-                   caption=f"👮‍♂ {message.from_user.mention} ɴᴏᴛɪᴄᴇ :ɪ𝙵 ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ 𝙵ɪʟᴇ𝚂 ᴏ𝙵 ᴛʜɪ𝚂 ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀ𝚂ᴋᴇᴅ 𝙵ᴏʀ. ʟᴏᴏᴋ ᴀᴛ ɴᴇ𝚇ᴛ ᴘᴀɢᴇ🔎\n©️քօաɛʀɛɖ ɮʏ :{message.chat.title}",
-                   photo="https://telegra.ph/file/8a8ba3e824e1d2482253f.jpg")
+            fmsg = await message.reply_photo(photo=imdb.get('poster'))
                    
                    
 
@@ -138,9 +136,7 @@ async def start_message(client, message):
 
         
 
-        fmsg = await message.reply_photo(
-               caption=f"👮‍♂ {message.from_user.mention} ɴᴏᴛɪᴄᴇ :ɪ𝙵 ʏᴏᴜ ᴅᴏ ɴᴏᴛ sᴇᴇ ᴛʜᴇ 𝙵ɪʟᴇ𝚂 ᴏ𝙵 ᴛʜɪ𝚂 ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀ𝚂ᴋᴇᴅ 𝙵ᴏʀ. ʟᴏᴏᴋ ᴀᴛ ɴᴇ𝚇ᴛ ᴘᴀɢᴇ🔎\n©️քօաɛʀɛɖ ɮʏ :{message.chat.title}",
-               photo="https://telegra.ph/file/8a8ba3e824e1d2482253f.jpg")
+        fmsg = await message.reply_photo(photo=imdb.get('poster'))
                
                
 
