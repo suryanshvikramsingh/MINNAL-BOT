3c0abdc3fddc45182dfac9941ab6e045fe27ce49
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 14:20:08 +0530

    Update image.py

--- plugins/MODULES/image.py
+++ plugins/MODULES/image.py
@@ -1,11 +1,7 @@
 import os
 import requests
 from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    InlineQueryResultPhoto
-)
+    
 from pyrogram import Client,filters
 import requests,os,wget
 from info import BATCH_GROUP, REQST_CHANNEL, SUPPORT_CHAT_ID, ADMINS
@@ -16,18 +12,14 @@ import asyncio
 
     
 @Client.on_message(filters.command("im") & filters.photo) 
-async def search_photo(client, message):
+async def photo(client, message):
     
-    results = requests.get(
-        API + requests.utils.requote_uri(update.search)
-    ).json()["result"][:50]
-
-
-
+    results = requests.get(API + requests.utils.requote_uri(update.search)).json()["result"][:50]
     try:
         await message.reply_photo(
-            photo=results,
-            title=update.search.capitalize(),
-            description=result)
+        photo=results,
+        title=update.search.capitalize(),
+        description=result
+    )
         
     
