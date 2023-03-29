7ca1c2fcd801d6446947cd70c0eb38d05d71fe32
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 18:43:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -74,7 +74,96 @@ async def rename(bot,update):
 	reply_to_message_id=update.message.reply_to_message.id,  
 	reply_markup=ForceReply(True))  
 
-
+@Client.on_callback_query(filters.regex("upload"))
+async def doc(bot, update):
+    type = update.data.split("_")[1]
+    new_name = update.message.text
+    new_filename = new_name.split(":-")[1]
+    file = update.message.reply_to_message
+    file_path = f"downloads/{new_filename}"
+    ms = await update.message.edit("\n༻☬ད 𝘽𝙪𝙞𝙡𝙙𝙞𝙣𝙜 𝙇𝙖𝙯𝙮 𝙈𝙚𝙩𝙖𝘿𝙖𝙩𝙖...")
+    c_time = time.time()
+    try:
+        path = await bot.download_media(
+                message=file,
+                progress=progress_for_pyrogram,
+                progress_args=("**\n  ღ♡ ꜰɪʟᴇ ᴜɴᴅᴇʀ ᴄᴏɴꜱᴛʀᴜᴄᴛɪᴏɴ... ♡♪**", ms, c_time))
+    except Exception as e:
+        await ms.edit(e)
+        return 
+    splitpath = path.split("/downloads/")
+    dow_file_name = splitpath[1]
+    old_file_name =f"downloads/{dow_file_name}"
+    os.rename(old_file_name, file_path)
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
+    user_id = int(update.message.chat.id) 
+    ph_path = None 
+    media = getattr(file, file.media.value)
+    filesize = humanize.naturalsize(media.file_size) 
+    c_caption = await db.get_caption(update.message.chat.id)
+    c_thumb = await db.get_thumbnail(update.message.chat.id)
+    if c_caption:
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
+         except Exception as e:
+             await ms.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
+             return 
+    else:
+        caption = f"**{new_filename}** \n\n⚡️Data costs: `{filesize}`"
+    if (media.thumbs or c_thumb):
+        if c_thumb:
+           ph_path = await bot.download_media(c_thumb) 
+        else:
+           ph_path = await bot.download_media(media.thumbs[0].file_id)
+        Image.open(ph_path).convert("RGB").save(ph_path)
+        img = Image.open(ph_path)
+        img.resize((320, 320))
+        img.save(ph_path, "JPEG")
+    await ms.edit("三 𝘗𝘳𝘦𝘱𝘢𝘳𝘪𝘯𝘨 𝘵𝘰 𝘳𝘦𝘤𝘦𝘪𝘷𝘦 𝘓𝘢𝘻𝘺 𝘧𝘪𝘭𝘦...︻デ═一")
+    c_time = time.time() 
+    try:
+       if type == "document":
+          await bot.send_document(
+	        update.message.chat.id,
+                   document=file_path,
+                   thumb=ph_path, 
+                   caption=caption, 
+                   progress=progress_for_pyrogram,
+                   progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time))
+       elif type == "video": 
+           await bot.send_video(
+	        update.message.chat.id,
+	        video=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time))
+       elif type == "audio": 
+           await bot.send_audio(
+	        update.message.chat.id,
+	        audio=file_path,
+	        caption=caption,
+	        thumb=ph_path,
+	        duration=duration,
+	        progress=progress_for_pyrogram,
+	        progress_args=( "**⎝⎝✧ ʀᴇᴄɪᴇᴠɪɴɢ ꜰɪʟᴇ ꜰʀᴏᴍ ʟᴀᴢʏ ꜱᴇʀᴠᴇʀ ✧⎠⎠**",  ms, c_time   )) 
+    except Exception as e: 
+        await ms.edit(f" Erro {e}") 
+        os.remove(file_path)
+        if ph_path:
+          os.remove(ph_path)
+        return 
+    await ms.delete() 
+    os.remove(file_path) 
+    if ph_path:
+       os.remove(ph_path)
 
 
 
