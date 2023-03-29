94d250088fcbeea1bda1144c057ea2f42fadc28c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 14:06:57 +0530

    Create image.py

---
+++ plugins/MODULES/image.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    InlineQueryResultPhoto
+)
+
+
+
+    results = requests.get
+        (API + requests.utils.requote_uri(update.query)
+    ).json()["result"][:50]
+
+@Client.on_message(filters.private & filters.photo & filters.command("im"]))
+async def search(bot, update):
+    
+    results = requests.get(
+        API + requests.utils.requote_uri(update.search)
+    ).json()["result"][:50]
+
+
+
+    for result in results:
+        await message.reply_audio(
+        title=update.search.capitalize(),
+        description=result,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
