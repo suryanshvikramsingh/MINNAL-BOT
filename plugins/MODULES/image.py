278b5de216e08251c516904792190b5880cf1f7c
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 14:23:22 +0530

    Update image.py

--- plugins/MODULES/image.py
+++ plugins/MODULES/image.py
@@ -7,11 +7,15 @@ import requests,os,wget
 from info import BATCH_GROUP, REQST_CHANNEL, SUPPORT_CHAT_ID, ADMINS
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import asyncio
-
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 
     
-@Client.on_message(filters.command("im") & filters.photo) 
+@Client.on_message(filters.photo) 
 async def photo(client, message):
     
     results = requests.get(API + requests.utils.requote_uri(update.search)).json()["result"][:50]
