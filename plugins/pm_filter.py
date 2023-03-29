ff34106f6284b162f9ed5956bcb71b6716afc3a3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 18:51:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -74,7 +74,7 @@ async def rename(bot,update):
 	reply_to_message_id=update.message.reply_to_message.id,  
 	reply_markup=ForceReply(True))  
 
-@Client.on_callback_query(filters.regex("upload"))
+@Client.on_message(filters.private & filters.command("up"))
 async def doc(bot, update):
     type = update.data.split("_")[1]
     new_name = update.message.text
