a7d77cc4d9a0efae486970c5399754d16df64443
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 16:09:08 +0530

    Update Saavn.py

--- plugins/MODULES/Saavn.py
+++ plugins/MODULES/Saavn.py
@@ -41,31 +41,32 @@ async def video(client, message):
     await client.send_message(LOG_CHANNEL, A.format(message.from_user.mention, message.from_user.id)) 
         
 
-@Client.on_message(filters.chat(CHAT_GROUP) & filters.command("/"))
+@Client.on_message(filters.command('saavn') & filters.text)
 async def song(client, message):
-    args = message.text.split(None)
-
-    r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
-#    sname = r['data']['results'][0]['name']
+    try:
+       args = message.text.split(None, 1)[1]
+    except:
+        return await message.reply("/saavn requires an argument.")
+    if args.startswith(" "):
+        await message.reply("/saavn requires an argument.")
+        return ""
+    pak = await message.reply('Downloading...')
+    try:
+        r = requests.get(f"https://saavn.me/search/songs?query={args}&page=1&limit=1").json()
+    except Exception as e:
+        await pak.edit(str(e))
+        return
+    sname = r['data']['results'][0]['name']
     slink = r['data']['results'][0]['downloadUrl'][4]['link']
     ssingers = r['data']['results'][0]['primaryArtists']
-#    album_id = r.json()[0]["albumid"]
+  #  album_id = r.json()[0]["albumid"]
     img = r['data']['results'][0]['image'][2]['link']
     thumbnail = wget.download(img)
     file = wget.download(slink)
     ffile = file.replace("mp4", "mp3")
     os.rename(file, ffile)
-    buttons = [[
-        InlineKeyboardButton("JOIN MOVIES", url="https://t.me/NASRANI_UPDATE")
-    ]]                           
-    await message.reply_audio(
-    audio=ffile, caption=f" from @nasrani_update ",
-    reply_markup=InlineKeyboardMarkup(buttons)
-)
-
+    await pak.edit('Uploading...')
+    await message.reply_audio(audio=ffile, title=sname, performer=ssingers,caption=f"[{sname}]({r['data']['results'][0]['url']}) - from saavn ",thumb=thumbnail)
     os.remove(ffile)
     os.remove(thumbnail)
-
-
-    await client.send_message(LOG_CHANNEL, A.format(message.from_user.mention, message.from_user.id)) 
-        
+    await pak.delete()
