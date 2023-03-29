ad757e342cf7c831ec935a6e9e7bbd3b73886424
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 20:09:42 +0530

    Update config.py

--- plugins/helpers/config.py
+++ plugins/helpers/config.py
@@ -8,3 +8,15 @@ CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+
+# mdisk
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MDISK_API = os.environ.get("MDISK_API")
+MDISK_CHANNEL = list(int(i.strip()) for i in os.environ.get("MDISK_CHANNEL").split(" ")) if os.environ.get("CHANNEL_ID") else []
+FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
+SOURCE_CODE = "💕SHARE AND SUPPORT💕"
+CHANNELS = bool(os.environ.get("CHANNELS"))
