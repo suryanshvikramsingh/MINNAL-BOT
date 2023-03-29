34a4b25f2936e3d48fd09421969b3587bfa29ac7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-29 17:08:21 +0530

    Update Rename.py

--- plugins/Rename.py
+++ plugins/Rename.py
@@ -30,6 +30,9 @@ import random
 from plugins.helpers.txt import mr
 from plugins.helpers.database import db
 from plugins.helpers.config import START_PIC, FLOOD, ADMINS
+from pyrogram import Client, filters
+from pyrogram.enums import MessageMediaType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 
 
 
@@ -58,24 +61,31 @@ async def start(client, message):
 
 @Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
 async def rename_start(client, message):
-    file = getattr(message, message.media.value)
-    filename = file.file_name
-    filesize = humanize.naturalsize(file.file_size) 
-    fileid = file.file_id
-    try:
-        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
-                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
-        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
-        await sleep(FLOOD)
-    except FloodWait as e:
-        await sleep(e.value)
-        text = f"""**__What do you want me to do with this file.?__**\n\n**File Name** :- `{filename}`\n\n**File Size** :- `{filesize}`"""
-        buttons = [[ InlineKeyboardButton("📝 𝚂𝚃𝙰𝚁𝚃 𝚁𝙴𝙽𝙰𝙼𝙴 📝", callback_data="rename") ],
-                   [ InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="cancel") ]]
-        await message.reply_text(text=text, reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(buttons))
-    except:
-        pass
+    reply_message = message.reply_to_message
+    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
+       new_name = message.text 
+       await message.delete() 
+       msg = await client.get_messages(message.chat.id, reply_message.id)
+       file = msg.reply_to_message
+       media = getattr(file, file.media.value)
+       if not "." in new_name:
+          if "." in media.file_name:
+              extn = media.file_name.rsplit('.', 1)[-1]
+          else:
+              extn = "mkv"
+          new_name = new_name + "." + extn
+       await reply_message.delete()
+
+       button = [[InlineKeyboardButton("📁 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃𝚂",callback_data = "upload_document")]]
+       if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
+           button.append([InlineKeyboardButton("🎥 𝚅𝙸𝙳𝙴𝙾",callback_data = "upload_video")])
+       elif file.media == MessageMediaType.AUDIO:
+           button.append([InlineKeyboardButton("🎵 𝙰𝙾𝚄𝙳𝙸𝙾",callback_data = "upload_audio")])
+       await message.reply_text(
+          f"**Select the output file type**\n**• File Name :-**```{new_name}```",
+          reply_to_message_id=file.id,
+          reply_markup=InlineKeyboardMarkup(button))
+
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
