1b27376ae4cb27778fdb72bb99cf3b970b8facc9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-30 00:11:25 +0530

    Update Join.py

--- plugins/EXTRA/Join.py
+++ plugins/EXTRA/Join.py
@@ -46,7 +46,7 @@ async def sub(client, message):
     reply_markup = InlineKeyboardMarkup(buttons)
     await message.reply_photo(
     photo=random.choice(SP),
-    caption=MESSAGE.format(message.text),
+    caption=ADS.format(message.text),
     reply_markup=reply_markup, 
     parse_mode=enums.ParseMode.HTML
     )
