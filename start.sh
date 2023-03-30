306fd8a45cccb9a415834d6987bdd5d048ad54e0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-31 00:16:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/COOL-BOT.git /COOL-BOT
+  git clone https://github.com/Azanpopz/MINNAL-BOT.git /MINNAL-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /COOL-BOT
+  git clone $UPSTREAM_REPO /MINNAL-BOT
 fi
-cd /COOL-BOT
+cd /MINNAL-BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
