3a9444f91e8014edf47d877eea6fee2ba3572223
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-30 14:03:43 +0530

    Update setbio.py

--- plugins/Users/setbio.py
+++ plugins/Users/setbio.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from info import IMDB_TEMPLATE
+from info import IMDB_TEMPLATE, SP
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
@@ -25,7 +25,7 @@ async def set_chat_description(bot, message):
 
 @Client.on_message(filters.command(["setdp"]))
 async def set_chat_bio(bot, message): 
-    await app.set_profile_photo(photo=random.choice(PICS))
+    await app.set_profile_photo(photo=random.choice(SP))
 
 
 
