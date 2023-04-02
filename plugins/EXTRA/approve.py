d8faf95daa9a84099b2be57eadde2670f7626d56
senapatibubai111 <120000603+senapatibubai111@users.noreply.github.com>
2023-04-03 00:07:49 +0530

    Update approve.py

--- plugins/EXTRA/approve.py
+++ plugins/EXTRA/approve.py
@@ -37,10 +37,9 @@ pr0fess0r_99=Client(
     api_hash = environ["API_HASH"]
 )
 
-AUTH_CHANNEL = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("AUTH_CHANNEL", None)]
+AUTH_CHANNEL = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("AUTH_CHANNEL", '').split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "ʜᴇʟʟᴏ {mention} ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴍʏ ᴄʜᴀɴɴᴇʟ. {title}\n\nᴏɴʟʏ ɴᴇᴡ ᴀɴᴅ ʟᴏᴡ ꜱɪᴢᴇ ᴍᴏᴠɪᴇ ᴀᴠᴀɪʟᴀʙʟᴇ. ᴇɴᴊᴏʏɪɴɢ🔥🔥")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
-
 @Client.on_message(filters.private & filters.command(["aprv"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
